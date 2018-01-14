fc3fdb12f21163feee6598ede6dbd1690b271163
Thomas Klausner <domm@plix.at>
2018-01-14 23:36:24 +0100

    simple wrapper script

---
+++ bin/pwgen-xkcd.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.010;
+
+# PODNAME: pwgen-xkcd.pl
+# ABSTRACT: Generate a xkcd-style password
+
+use CtrlO::Crypt::XkcdPassword;
+use Getopt::Long;
+use Pod::Usage qw(pod2usage);
+
+binmode STDOUT, ":utf8";
+
+my $words = 4;
+my $digits = 0;
+my $help = 0;
+
+GetOptions ( "words=i" => \$words, "digits=i" => \$digits, "help|?" => \$help );
+pod2usage(1)  if ($help);
+
+say CtrlO::Crypt::XkcdPassword->new->xkcd({ words=>$words, digits=> $digits });
+
+__END__
+
+=head1 USAGE
+
+  pwgen-xkcd.pl [options]
+
+  Options:
+    --words      Number of words to generate, default 4
+    --digits     Add some digits, default 0
+
+=head1 SEE ALSO
+
+See C<perldoc CtrlO::Crypt::XkcdPassword> for even more info.
